348dbdabf6d809f1d386addf56ebc57af0e81130
Spell master <spellmastergames@gmail.com>
2017-03-11 21:53:16 -0300

    Adicionado aeroplano

---
+++ NpcScript/Transportes/Aeroplano.c
@@ -0,0 +1,279 @@
+/*-----------------------------------------------------------------*\
+|             ______ ____ _____ ___   __                            |
+|            / ____ / _  / ____/  /  /  /                           |
+|            \___  /  __/ __/ /  /__/  /___                         |
+|           /_____/_ / /____//_____/______/                         |
+|                /\  /|   __    __________ _________                |
+|               /  \/ |  /  |  /  ___  __/ ___/ _  /                |
+|              /      | / ' | _\  \ / / / __//  __/                 |
+|             /  /\/| |/_/|_|/____//_/ /____/_/\ \                  |
+|            /__/   |_|      npc Script         \/                  |
+|                                                                   |
++-------------------------------------------------------------------+
+|                     Projeto Ragnarok Online                       |
++-------------------------------------------------------------------+
+| - Por: eAthena/rAthena/Hercules                                   |
+| - Versão: Spell Master                                            |
+| - Nota: Controle sobre as rotas dos dirigiveis                    |
+\*-----------------------------------------------------------------*/
+
+//-------------------------------------------------------------------
+// - [ Rota: Juno, Einbroch, Lighthalzen, Hugel ] -
+//-------------------------------------------------------------------
+airplane,243,73,0	script	#AirshipWarp-1	WARPNPC,1,1,{
+	end;
+
+	OnInit:
+	OnHide:
+	specialeffect EF_BASH;
+	disablenpc strnpcinfo(NPC_NAME);
+	end;
+
+	OnUnhide:
+	enablenpc strnpcinfo(NPC_NAME);
+	specialeffect EF_SUMMONSLAVE;
+	end;
+
+	OnTouch:
+	switch ($@airplanelocation) {
+		case 0: warp "yuno",92,260; end;
+		case 1: warp "einbroch",92,278; end;
+		case 2: warp "lighthalzen",302,75; end;
+		case 3: warp "hugel",181,146; end;
+	}
+}
+
+airplane,243,29,0	duplicate(#AirshipWarp-1)	#AirshipWarp-2	WARPNPC,1,1
+
+airplane,1,1,0	script	Aeroplano_Domestico	HIDDEN_NPC,{
+	end;
+	OnInit:
+	initnpctimer;
+	end;
+	OnTimer20000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Einbroch.",bc_map,"0x00ff00";
+	end;
+	OnTimer50000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Einbroch.",bc_map,"0x00ff00";
+	end;
+	OnTimer60000:
+	$@airplanelocation = 1;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Einbroch. Tenha uma boa viagem.",bc_map,"0x00ff00";
+	end;
+	OnTimer70000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Einbroch. O Aeroplano vai decolar em breve.",bc_map,"0x00ff00";
+	end;
+	OnTimer80000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está decolando. Nosso próximo destino é Lighthalzen.",bc_map,"0x70dbdb";
+	end;
+	OnTimer100000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Lighthalzen.",bc_map,"0x70dbdb";
+	end;
+	OnTimer130000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Lighthalzen.",bc_map,"0x70dbdb";
+	end;
+	OnTimer140000:
+	$@airplanelocation = 2;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Lighthalzen. Tenha uma boa viagem.",bc_map,"0x70dbdb";
+	end;
+	OnTimer150000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Lighthalzen. O Aeroplano vai decolar em breve.",bc_map,"0x70dbdb";
+	end;
+	OnTimer160000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está deixando o solo. Nosso próximo destino é Einbroch.",bc_map,"0x00ff00";
+	end;
+	OnTimer180000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Einbroch.",bc_map,"0x00ff00";
+	end;
+	OnTimer210000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Einbroch.",bc_map,"0x00FF00";
+	end;
+	OnTimer220000:
+	$@airplanelocation = 1;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Einbroch. Tenha uma boa viagem.",bc_map,"0x00ff00";
+	end;
+	OnTimer230000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Einbroch. O Aeroplano vai decolar em breve.",bc_map,"0x00ff00";
+	end;
+	OnTimer240000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está decolando. Nosso próximo destino é Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer260000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer290000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer300000:
+	$@airplanelocation = 0;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Juno. Tenha uma boa viagem.",bc_map,"0xff8200";
+	end;
+	OnTimer310000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Juno. O Aeroplano sairá em breve.",bc_map,"0xff8200";
+	end;
+	OnTimer320000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está deixando o solo. Nosso próximo destino é Hugel.",bc_map,"0xca4bf3";
+	end;
+	OnTimer340000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Hugel.",bc_map,"0xca4bf3";
+	end;
+	OnTimer370000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Hugel.",bc_map,"0xca4bf3";
+	end;
+	OnTimer380000:
+	$@airplanelocation = 3;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Hugel. Tenha uma boa viagem.",bc_map,"0xca4bf3";
+	end;
+	OnTimer390000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Hugel. O Aeroplano sairá em breve.",bc_map,"0xca4bf3";
+	end;
+	OnTimer400000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está deixando o solo. Nosso próximo destino é Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer420000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer450000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Juno.",bc_map,"0xff8200";
+	end;
+	OnTimer460000:
+	$@airplanelocation = 0;
+	donpcevent "#AirshipWarp-1::OnUnhide";
+	donpcevent "#AirshipWarp-2::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Juno. Tenha uma boa viagem.",bc_map,"0xff8200";
+	end;
+	OnTimer470000:
+	mapannounce strnpcinfo(NPC_MAP),"Atualmente estamos em Juno. O Aeroplano sairá em breve.",bc_map,"0xff8200";
+	end;
+	OnTimer480000:
+	donpcevent "#AirshipWarp-1::OnHide";
+	donpcevent "#AirshipWarp-2::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está deixando o solo. Nosso próximo destino é Einbroch.",bc_map,"0x00ff00";
+	stopnpctimer;
+	initnpctimer;
+}
+
+//-------------------------------------------------------------------
+// - [ Rota: Izlude, Yuno ] -
+//-------------------------------------------------------------------
+airplane_01,243,73,0	script	#AirshipWarp-3	WARPNPC,1,1,{
+	end;
+
+	OnTouch:
+	switch ($@airplanelocation2) {
+		case 0: warp "ra_fild12",292,204; end;
+		case 1: warp "izlude",200,73; end;
+		case 2: warp "yuno",12,261; end;
+	}
+
+	OnInit:
+	OnHide:
+	specialeffect EF_BASH;
+	disablenpc strnpcinfo(PC_NAME);
+	end;
+
+	OnUnhide:
+	enablenpc strnpcinfo(PC_NAME);
+	specialeffect EF_SUMMONSLAVE;
+	end;
+}
+
+airplane_01,243,29,0	duplicate(#AirshipWarp-3)	#AirshipWarp-4	WARPNPC,1,1
+
+airplane_01,1,1,0	script	International_Airship	HIDDEN_NPC,{
+	end;
+	OnInit:
+	OnEnable:
+	initnpctimer;
+	end;
+	OnTimer25000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Izlude.",bc_map,"0x00ff00";
+	end;
+	OnTimer50000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Izlude.",bc_map,"0x00ff00";
+	end;
+	OnTimer60000:
+	$@airplanelocation2 = 1;
+	donpcevent "#AirshipWarp-3::OnUnhide";
+	donpcevent "#AirshipWarp-4::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Izlude. Tenha uma boa viagem.",bc_map,"0x00ff00";
+	end;
+	OnTimer70000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos atualmente em Izlude. O Aeroplano vai decolar em breve.",bc_map,"0x00ff00";
+	end;
+	OnTimer80000:
+	donpcevent "#AirshipWarp-3::OnHide";
+	donpcevent "#AirshipWarp-4::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está decolando. Nosso próximo destino é Juno.",bc_map,"0x70dbdb";
+	end;
+	OnTimer105000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Juno.",bc_map,"0x70dbdb";
+	end;
+	OnTimer130000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Juno.",bc_map,"0x70dbdb";
+	end;
+	OnTimer140000:
+	$@airplanelocation2 = 2;
+	donpcevent "#AirshipWarp-3::OnUnhide";
+	donpcevent "#AirshipWarp-4::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Juno. Tenha uma boa viagem.",bc_map,"0x70dbdb";
+	end;
+	OnTimer150000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos atualmente em Juno. O Aeroplano sairá em breve.",bc_map,"0x70dbdb";
+	end;
+	OnTimer160000:
+	donpcevent "#AirshipWarp-3::OnHide";
+	donpcevent "#AirshipWarp-4::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está deixando o solo. Nosso próximo destino é Rachel.",bc_map,"0xFF8200";
+	end;
+	OnTimer185000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos indo para Rachel.",bc_map,"0xFF8200";
+	end;
+	OnTimer210000:
+	mapannounce strnpcinfo(NPC_MAP),"Logo chegaremos em Rachel.",bc_map,"0xFF8200";
+	end;
+	OnTimer220000:
+	$@airplanelocation2 = 0;
+	donpcevent "#AirshipWarp-3::OnUnhide";
+	donpcevent "#AirshipWarp-4::OnUnhide";
+	mapannounce strnpcinfo(NPC_MAP),"Bem-vindo(a) a Rachel. Tenha uma boa viagem.",bc_map,"0xFF8200";
+	end;
+	OnTimer230000:
+	mapannounce strnpcinfo(NPC_MAP),"Estamos atualmente em Rachel. O Aeroplano vai decolar em breve.",bc_map,"0xFF8200";
+	end;
+	OnTimer240000:
+	donpcevent "#AirshipWarp-3::OnHide";
+	donpcevent "#AirshipWarp-4::OnHide";
+	mapannounce strnpcinfo(NPC_MAP),"O Aeroplano está decolando. Nosso próximo destino é Izlude.",bc_map,"0x00ff00";
+	stopnpctimer;
+		/*++.moninv;
+		if (.moninv == 7) {
+			if (rand(1,3) == 3) {
+				donpcevent "Airship#air02::OnEnable";
+				end;
+			}
+			.moninv = 0;
+		}*/
+	initnpctimer;
+	end;
+}
---
+++ NpcScript/Transportes/Embarque_do_Aeroplano.c
@@ -0,0 +1,136 @@
+/*-----------------------------------------------------------------*\
+|             ______ ____ _____ ___   __                            |
+|            / ____ / _  / ____/  /  /  /                           |
+|            \___  /  __/ __/ /  /__/  /___                         |
+|           /_____/_ / /____//_____/______/                         |
+|                /\  /|   __    __________ _________                |
+|               /  \/ |  /  |  /  ___  __/ ___/ _  /                |
+|              /      | / ' | _\  \ / / / __//  __/                 |
+|             /  /\/| |/_/|_|/____//_/ /____/_/\ \                  |
+|            /__/   |_|      npc Script         \/                  |
+|                                                                   |
++-------------------------------------------------------------------+
+|                     Projeto Ragnarok Online                       |
++-------------------------------------------------------------------+
+| - Por: Spell Master                                               |
+| - Nota: Npcs de embarque e desembarque nos aeroplanos             |
+\*-----------------------------------------------------------------*/
+
+//-------------------------------------------------------------------
+// - [Embarque] -
+//-------------------------------------------------------------------
+-	script	EnterAirship	FAKE_NPC,{
+	mes "[Embarque do Aeroporto]";
+	mes "Bem-vind"+(Sex?"o":"a")+" ao serviço de embarque do Aeroplano.";
+	mes "A taxa do embarque é de 1,200 zeny.";
+	mes "Gostaria de utilizar nossos serviços?";
+	next;
+	if (select("Embarcar.:Cancelar.") == 1) {
+		if (Zeny >= 1200) {
+			Zeny -= 1200;
+			if (strnpcinfo(NPC_MAP)      == "airport")     { warp "airport",148,51; }
+			else if (strnpcinfo(NPC_MAP) == "lhz_airport") { warp "lhz_airport",148,51; }
+			else if (strnpcinfo(NPC_MAP) == "y_airport")   { warp "y_airport",148,51; }
+			else if (strnpcinfo(NPC_MAP) == "izlude")      { warp "airplane_01",244,58; }
+			else if (strnpcinfo(NPC_MAP) == "hugel")       { warp "airplane",244,58; }
+			end;
+		}
+		mes "[Embarque do Aeroporto]";
+		mes "Desculpe mas a taxa de embarque custa 1,200 zenys.";
+		mes "Volte quando tiver mais dinheiro.";
+		close;
+	}
+	mes "[Embarque do Aeroporto]";
+	mes "Tudo bem, tenha um bom dia.";
+	close;
+}
+
+// - airport (Einbroich)
+airport,143,43,5	duplicate(EnterAirship)	Funcionária#airportenter1	4_F_01
+airport,158,43,5	duplicate(EnterAirship)	Funcionária#airportenter2	4_F_01
+airport,126,43,5	duplicate(EnterAirship)	Funcionária#airportenter3	4_F_01
+// - lhz_airport (Lighthalzen)
+lhz_airport,143,43,5	duplicate(EnterAirship)	Funcionária#lhzairenter1	4_F_01
+lhz_airport,158,43,5	duplicate(EnterAirship)	Funcionária#lhzairenter2	4_F_01
+lhz_airport,126,43,5	duplicate(EnterAirship)	Funcionária#lhzairenter3	4_F_01
+// - y_airport (Juno/Yuno)
+y_airport,143,43,5	duplicate(EnterAirship)	Funcionária#yairenter1	4_F_01
+y_airport,158,43,5	duplicate(EnterAirship)	Funcionária#yairenter2	4_F_01
+y_airport,126,43,5	duplicate(EnterAirship)	Funcionária#yairenter3	4_F_01
+// - Izlude
+izlude,206,55,3	duplicate(EnterAirship)	Funcionária#izlairenter	4_F_01
+// - Hugel
+hugel,178,142,0	duplicate(EnterAirship)	Funcionária#hugairenter	4_F_01
+
+//---------------------------------------------------------------
+// - [Terminal de Juno] -
+//---------------------------------------------------------------
+y_airport,145,63,5	script	Voos Domésticos	4_F_02,{
+	mes "[Equipe de Bordo]";
+	mes "Você gostaria de embarcar no dirigível que voa até Einbroch, Lighthalzen e Hugel?";
+	mes "Se sim, deixe que eu te guie até a área de embarque.";
+	next;
+	if (select("Sim:Não") == 1) {
+		warp "yuno",59,244;
+		end;
+	}
+	mes "[Equipe de Bordo]";
+	mes "Muito bem, então.";
+	mes "Obrigad"+(Sex?"o":"a")+" por se tornar um cliente, espero que tenha uma viajem divertida.";
+	close;
+}
+
+y_airport,140,63,5	script	Voos Internacionais	4_F_02,{
+	mes "[Equipe de Bordo]";
+	mes "Você deseja embarcar no dirigível que leva até Juno, Izlude e Rachel?";
+	mes "Se sim, deixe que eu te guie até a área de embarque.";
+	next;
+	if (select("Sim:Não") == 1) {
+		warp "yuno",47,244;
+		end;
+	}
+	mes "[Equipe de Bordo]";
+	mes "Muito bem, então.";
+	mes "Obrigad"+(Sex?"o":"a")+" por voar conosco, espero uma viajem divertida.";
+	close;
+}
+
+//---------------------------------------------------------------
+// - [Desembarque] -
+//---------------------------------------------------------------
+-	script	ExitAirship	FAKE_NPC,{
+	mes "[Desembarque do Aeroporto]";
+	mes "Deseja sair do terminal de embarque?";
+	next;
+	if (select("Desembarcar:Ficar no terminal") == 1) {
+		mes "[Desembarque do Aeroporto]";
+		mes "Se você sair agora do terminal, terá que pagar a taxa de embarque novamente.";
+		mes "Tem certeza que deseja sair?";
+		if (select("Sim:Não") == 1) {
+			if (strnpcinfo(NPC_MAP)      == "airport")     { warp "airport",142,40; }
+			else if (strnpcinfo(NPC_MAP) == "lhz_airport") { warp "lhz_airport",142,40; }
+			else if (strnpcinfo(NPC_MAP) == "y_airport")   { warp "y_airport",142,40; }
+		}
+		mes "[Desembarque do Aeroporto]";
+		mes "Tudo bem, fique o tempo que desejar.";
+		close;
+	}
+	mes "[Desembarque do Aeroporto]";
+	mes "Tudo bem, fique o tempo que desejar.";
+	close;
+}
+
+// - airport (Einbroch)
+airport,143,49,3	duplicate(ExitAirship)	Recepcionista#airportexit1	4_F_01
+airport,126,51,3	duplicate(ExitAirship)	Recepcionista#airportexit2	4_F_01
+airport,158,50,3	duplicate(ExitAirship)	Recepcionista#airportexit3	4_F_01
+
+// - lhz_airport (Lighthalzen)
+lhz_airport,143,49,3	duplicate(ExitAirship)	Recepcionista#lhzairexit1	4_F_01
+lhz_airport,126,51,3	duplicate(ExitAirship)	Recepcionista#lhzairexit2	4_F_01
+lhz_airport,158,50,3	duplicate(ExitAirship)	Recepcionista#lhzairexit3	4_F_01
+
+// - y_airport (Juno/Yuno)
+y_airport,143,49,3	duplicate(ExitAirship)	Recepcionista#yairexit1	4_F_01
+y_airport,126,51,3	duplicate(ExitAirship)	Recepcionista#yairexit2	4_F_01
+y_airport,158,50,3	duplicate(ExitAirship)	Recepcionista#yairexit3	4_F_01
