3750091d9c778e9de9db1ce2892a9402c399337f
Spell Master <spellmastergames@gmail.com>
2017-04-02 23:02:01 -0300

    Outros portais

---
+++ NpcScript/Portais/Outros/Aeroplano.c
@@ -0,0 +1,60 @@
+/*-----------------------------------------------------------------*\
+|             ______ ____ _____ ___   __                            |
+|            / ____ / _  / ____/  /  /  /                           |
+|            \___  /  __/ __/ /  /__/  /___                         |
+|           /_____/_ / /____//_____/______/                         |
+|                /\  /|   __    __________ _________                |
+|               /  \/ |  /  |  /  ___  __/ ___/ _  /                |
+|              /      | / ' | _\  \ / / / __//  __/                 |
+|             /  /\/| |/_/|_|/____//_/ /____/_/\ \                  |
+|            /__/   |_|      npc Script         \/                  |
+|                                                                   |
++-------------------------------------------------------------------+
+|                     Projeto Ragnarok Online                       |
++-------------------------------------------------------------------+
+| - Versão: Spell Master                                            |
+| - Nota: Potais nos Aeroportos e Aeroplano                         |
+\*-----------------------------------------------------------------*/
+
+// ------------------------------------------------------------------
+// - [ Aeroportos ] -
+// ------------------------------------------------------------------
+airport,142,61,0	warp	ein_ap02a	1,1,einbroch,62,246
+airport,124,13,0	warp	ein_ap03	1,1,airport,19,21
+airport,19,18,0	warp	ein_ap03a	1,1,airport,122,16
+airport,161,13,0	warp	ein_ap04	1,1,airport,47,21
+airport,47,18,0	warp	ein_ap04a	1,1,airport,163,16
+airport,143,14,0	warp	ein_ap05	1,1,einbroch,64,204
+
+// ------------------------------------------------------------------
+lhz_airport,125,14,0	warp	lairp1	1,1,lhz_airport,19,20
+lhz_airport,19,18,0	warp	lairp1a	1,1,lhz_airport,123,14
+lhz_airport,160,14,0	warp	lairp2	1,1,lhz_airport,48,20
+lhz_airport,48,18,0	warp	lairp2a	1,1,lhz_airport,162,14
+lhz_airport,143,13,0	warp	lairp3a	1,1,lighthalzen,265,76
+lhz_airport,142,60,0	warp	lairp4	1,1,lighthalzen,296,76
+
+// ------------------------------------------------------------------
+y_airport,125,14,0	warp	jairp1	1,1,y_airport,19,20
+y_airport,19,18,0	warp	jairp1a	1,1,y_airport,123,14
+y_airport,160,14,0	warp	jairp2	1,1,y_airport,48,20
+y_airport,48,18,0	warp	jairp2a	1,1,y_airport,162,14
+y_airport,143,16,0	warp	jairp3a	1,1,yuno,52,207
+
+// ------------------------------------------------------------------
+// - [ Aeroplanos ] -
+// ------------------------------------------------------------------
+airplane,254,54,0	warp	air_1wp01	1,1,airplane,91,67
+airplane,87,67,0	warp	air_1wp01a	1,1,airplane,250,54
+airplane,208,53,0	warp	air_1wp02	1,1,airplane,239,160
+airplane,245,160,0	warp	air_1wp02a	1,1,airplane,214,54
+
+// ------------------------------------------------------------------
+airplane_01,254,54,0	warp	air_2wp01	1,1,airplane_01,91,67
+airplane_01,87,67,0	warp	air_2wp01a	1,1,airplane_01,250,54
+airplane_01,208,53,0	warp	air_2wp02	1,1,airplane_01,239,160
+airplane_01,245,160,0	warp	air_2wp02a	1,1,airplane_01,214,54
+airplane_01,104,199,0	warp	air_2wp03	1,1,airplane_01,105,72
+airplane_01,103,72,0	warp	air_2wp03a	1,1,airplane_01,102,199
+airplane_01,104,176,0	warp	air_2wp04	1,1,airplane_01,105,52
+airplane_01,103,52,0	warp	air_2wp04a	1,1,airplane_01,102,176
---
+++ NpcScript/Portais/Outros/Classes.c
@@ -0,0 +1,116 @@
+/*-----------------------------------------------------------------*\
+|             ______ ____ _____ ___   __                            |
+|            / ____ / _  / ____/  /  /  /                           |
+|            \___  /  __/ __/ /  /__/  /___                         |
+|           /_____/_ / /____//_____/______/                         |
+|                /\  /|   __    __________ _________                |
+|               /  \/ |  /  |  /  ___  __/ ___/ _  /                |
+|              /      | / ' | _\  \ / / / __//  __/                 |
+|             /  /\/| |/_/|_|/____//_/ /____/_/\ \                  |
+|            /__/   |_|      npc Script         \/                  |
+|                                                                   |
++-------------------------------------------------------------------+
+|                     Projeto Ragnarok Online                       |
++-------------------------------------------------------------------+
+| - Versão: Spell Master                                            |
+| - Nota: Potais nas Areas de Mudança de Classe                     |
+\*-----------------------------------------------------------------*/
+
+// ------------------------------------------------------------------
+// - [ Monje ] -
+// ------------------------------------------------------------------
+monk_in,128,46,0	warp	monk03	1,3,monk_in,161,38
+monk_in,128,84,0	warp	monk04	1,3,monk_in,161,90
+monk_in,159,38,0	warp	monk03-1	1,3,monk_in,126,46
+monk_in,159,90,0	warp	monk04-1	1,3,monk_in,126,84
+monk_in,40,38,0	warp	monk05	1,3,monk_in,71,46
+monk_in,40,92,0	warp	monk06	1,3,monk_in,71,84
+monk_in,69,46,0	warp	monk05-1	1,3,monk_in,38,38
+monk_in,69,84,0	warp	monk06-1	1,3,monk_in,38,92
+monk_in,98,186,0	warp	monk02-1	3,2,prt_monk,245,139
+monk_in,98,27,0	warp	monk01-1	5,2,prt_monk,245,104
+monk_in,99,102,0	warp	monk07	5,1,monk_in,99,143
+monk_in,99,141,0	warp	monk07-1	3,1,monk_in,99,100
+
+// ------------------------------------------------------------------
+prt_monk,22,248,0	warp	monk001	2,2,prt_fild03,368,256
+prt_monk,245,106,0	warp	monk01	1,1,monk_in,98,30
+prt_monk,245,137,0	warp	monk02	1,1,monk_in,98,183
+prt_monk,192,172,0	warp	monk15	1,1,monk_test,329,50
+
+// ------------------------------------------------------------------
+monk_test,329,47,0	warp	monk16	1,1,prt_monk,193,166
+monk_test,329,76,0	warp	monk17	1,1,monk_test,259,118
+monk_test,259,115,0	warp	monk18	1,1,monk_test,329,71
+monk_test,272,125,0	warp	monk19	1,1,monk_test,301,127
+monk_test,298,127,0	warp	monk20	1,1,monk_test,268,125
+
+// ------------------------------------------------------------------
+// - [ Gaturno ] -
+// ------------------------------------------------------------------
+job_thief1,180,15,0	warp	jthf	7,1,moc_ruins,145,117
+
+// ------------------------------------------------------------------
+// - [ Mercenário ] -
+// ------------------------------------------------------------------
+in_moc_16,18,8,0	warp	guild_to_16	2,2,moc_fild16,205,291
+
+// ------------------------------------------------------------------
+// - [ Caçador ] -
+// ------------------------------------------------------------------
+in_hunter,100,15,0	warp	jhun01	0,0,pay_fild10,143,250
+
+// ------------------------------------------------------------------
+// - [ Alquimista ] -
+// ------------------------------------------------------------------
+alde_alche,19,171,0	warp	alche#1-1	1,1,aldebaran,68,56
+alde_alche,42,171,0	warp	alche#1-2	1,1,aldebaran,56,68
+alde_alche,41,186,0	warp	alche#1F_2F	1,1,alde_alche,113,178
+alde_alche,114,183,0	warp	alche#2F_1F	1,1,alde_alche,42,182
+alde_alche,13,184,0	warp	alche#1F_B1	1,1,alde_alche,88,113
+alde_alche,88,117,0	warp	alche#B1_1F	1,1,alde_alche,13,181
+alde_alche,46,104,0	warp	alche#B1_r1	1,1,alde_alche,157,17
+alde_alche,160,17,0	warp	alche#r1_b1	1,1,alde_alche,50,103
+alde_alche,46,77,0	warp	alche#B1_r2	1,1,alde_alche,88,17
+alde_alche,93,17,0	warp	alche#r2_b1	1,1,alde_alche,50,77
+alde_alche,89,62,0	warp	alche#B1_r3	1,1,alde_alche,17,23
+alde_alche,17,29,0	warp	alche#r3_b1	1,1,alde_alche,89,67
+alde_alche,133,77,0	warp	alche#B1_r4	1,1,alde_alche,162,107
+alde_alche,158,107,0	warp	alche#r4_b1	1,1,alde_alche,129,77
+alde_alche,133,103,0	warp	alche#B1_r5	1,1,alde_alche,164,164
+alde_alche,158,163,0	warp	alche#r5_b1	1,1,alde_alche,129,103
+
+// ------------------------------------------------------------------
+// - [ Arruaceiro ] -
+// ------------------------------------------------------------------
+in_rogue,375,46,0	warp	rogue00-fild07	1,1,cmd_fild07,196,117
+in_rogue,375,34,0	warp	rogue01-02	1,1,in_rogue,380,125
+in_rogue,375,125,0	warp	rogue02-01	1,1,in_rogue,379,33
+in_rogue,247,122,0	warp	rogue03-fild07	1,1,cmd_fild07,349,285
+in_rogue,244,21,0	warp	rogue04-fild09	1,1,cmd_fild09,106,191
+in_rogue,172,34,0	warp	rogue05-fild09	1,1,cmd_fild09,341,143
+in_rogue,161,103,0	warp	rogue06-fild04	1,1,cmd_fild04,302,177
+in_rogue,8,9,0	warp	stair_to_ro	1,1,in_rogue,389,389
+
+// ------------------------------------------------------------------
+// - [ Odalisca ] -
+// ------------------------------------------------------------------
+job_duncer,69,43,0	warp	duncer1	1,1,comodo,185,156
+
+// ------------------------------------------------------------------
+// - [ Justiceiro ] -
+// ------------------------------------------------------------------
+que_ng,130,166,0	warp	gswarp2	1,1,einbroch,130,197
+que_ng,178,162,0	warp	gswarp3	1,1,que_ng,172,86
+que_ng,166,85,0	warp	gswarp4	1,1,que_ng,182,161
+que_ng,165,137,0	warp	gswarp5	1,1,que_ng,177,41
+que_ng,172,42,0	warp	gswarp6	1,1,que_ng,160,139
+
+// ------------------------------------------------------------------
+// - [ Ninja ] -
+// ------------------------------------------------------------------
+que_ng,20,138,0	warp	ninja02	1,1,amatsu,147,136
+que_ng,10,183,0	warp	ninja03	1,1,que_ng,33,64
+que_ng,37,64,0	warp	ninja04	1,1,que_ng,15,182
+que_ng,30,37,0	warp	ninja05	1,1,que_ng,67,28
+que_ng,63,30,0	warp	ninja06	1,1,que_ng,25,37
